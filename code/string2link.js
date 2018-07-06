379ceb77b7b0228d64b52f92d364ba3679157f0d
Whitefur Wang <whitefur.wang@gmail.com>
2018-07-06 12:49:13 +0800

    Change codes section code order. Remove code 'string2link' unused parameter.

--- code/string2link.js
+++ code/string2link.js
@@ -1,7 +1,7 @@
 const string2link = string => {
   const pattern = /(\[{1}[^\][]{1,}\])(\([^)]{1,}\))/g
   const trim = str => str.substring(1, str.length - 1)
-  const replacer = (match, p1, p2, offset, string) => (
+  const replacer = (match, p1, p2) => (
     `<a href="${trim(p2)}" target="_blank">${trim(p1)}</a>`
   )
 
